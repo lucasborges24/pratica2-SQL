76bae41f5d37cb902b8679574bebf1d17513d2ad
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-28 14:56:47 -0300

    feat: add 2nd exercise answer

--- practice.sql
+++ practice.sql
@@ -1,3 +1,14 @@
 -- Exercise 1
-SELECT COUNT("endDate") AS "currentExperiences"
-FROM experiences;
+SELECT
+    COUNT("endDate") AS "currentExperiences"
+FROM
+    experiences;
+
+-- Exercise 2
+SELECT
+    "userId" AS "id",
+    COUNT("id") AS "educations"
+FROM
+    educations
+GROUP BY
+    "userId";
